3408146e1cecb852e4943b39e5e974382ef5a1c6
vasileiosl993 <80270208+vasileiosl993@users.noreply.github.com>
2022-05-18 20:19:55 +0300

    Add files via upload

---
+++ src/create_table.sql
@@ -0,0 +1,16 @@
+CREATE TABLE ecg (
+  id integer PRIMARY KEY,
+  name char(30),
+  city char(30),
+  date_of_testing date,
+  seg1 box,
+  seg2 box,
+  seg3 box,
+  seg4 box,
+  seg5 box,
+  seg6 box,
+  seg7 box,
+  seg8 box,
+  seg9 box,
+  seg10 box
+);
